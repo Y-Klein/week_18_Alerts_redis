cb37800d9c4ce172fb1d357b690517a1ed4f1725
Y-Klein <k0556714698@gmail.com>
2026-02-19 12:53:09 +0200

    sum chenges

--- analytics-api/mongo_connection.py
+++ analytics-api/mongo_connection.py
@@ -1,7 +1,7 @@
 import pymongo
 
 def get_mongo_con():
-    my_client = pymongo.MongoClient("mongodb://localhost:27017/")
+    my_client = pymongo.MongoClient("mongodb://mongo:27017/")
     my_db = my_client["mydatabase"]
-    my_col = my_db["customers"]
+    my_col = my_db["alerts"]
     return my_col
